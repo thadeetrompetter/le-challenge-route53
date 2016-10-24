9a1baebf8e443987c7e152e2cb7a3e88fc5aa0dd
Thadee Trompetter <thadee@voorhoede.nl>
2016-10-24 23:04:04 +0200

    basic set & remove dns TXT record @ route53

--- index.js
+++ index.js
@@ -1,13 +1,48 @@
 'use strict';
 
-const Challenge = module.exports;
-const route53 = new require('aws-sdk')
-  .Route53({apiVersion: '2013-04-01'});
+const fs = require('fs');
+const base64url = require('base64-url');
+const AWS = require('aws-sdk');
+const R = require('ramda');
+const route53 = new AWS.Route53({apiVersion: '2013-04-01'});
 
-Challenge.create = function (defaults) {
+const Challenge = module.exports;
+
+const defaults = {
+  debug: false,
+  // _acme-challenge.example.com TXT xxxxxxxxxxxxxxxx
+  acmeChallengeDns: '_acme-challenge.',
+  AWSConfigFile: './config.json'
+};
+
+// store domain name, zone ID and digest for each dns record here to share
+// between method calls
+// TODO: consider persistent storage
+const store = {
+  getPayload: function (domain) {
+    return Promise.resolve(this[domain] || null);
+  },
+  setPayload: function (domain, payload) {
+    this[domain] = payload;
+    return Promise.resolve();
+  }
+};
+
+Challenge.create = function (options) {
+  const opts = mergeOptions(defaults, options);
+
+  // AWS authentication is loaded from config file if its path is provided and
+  // the file exists.
+  if(opts.AWSConfigFile && fs.existsSync(opts.AWSConfigFile)){
+    // TODO: commented out while debugging
+    // AWS.config.loadFromPath(opts.AWSConfigFile);
+  }
+  // Configure AWS api to return (native) promises
+  AWS.config.setPromisesDependency(Promise);
+
   return {
     getOptions: function () {
-      return defaults || {};
+      return Object.assign({}, defaults) ;
     },
     set: Challenge.set,
     get: Challenge.get,
@@ -17,25 +52,106 @@ Challenge.create = function (defaults) {
   };
 };
 
-function route53Payload(action, domain, value, id) {
+function getChallengeDomain(domain) {
+  return `_acme-challenge.${domain}`;
+}
+function getAWSConfig(location) {
+  try {
+    JSON.parse(fs.readFileSync(location));
+  } catch(err) {
+    return null;
+  };
+}
+
+function mergeOptions(defaults, options) {
+  return Object.assign({}, defaults, options);
+}
+
+function route53BasePayload(action, id) {
   return {
     ChangeBatch: {
       Changes: [{
-        Action: action,
-        ResourceRecordSet: {
-          Name: getChallengeDomain(domain),
-          Type: 'TXT',
-          TTL: 60,
-          ResourceRecords: [{
-            Value: `"${value}"`
-          }]
-        }
+        Action: action
       }]
     },
     HostedZoneId: id
   };
 }
+
+function route53Payload(action, id, domain, value) {
+    const payload = route53BasePayload(action, id);
+    const {ChangeBatch: { Changes: [ change ] }} = payload;
+    change.ResourceRecordSet = getResourceRecordSet(domain, [
+      getValue(value)
+    ]);
+    return payload;
+}
+
+const route53CreatePayload = R.curry(route53Payload)('CREATE');
+const route53DeletePayload = R.curry(route53Payload)('DELETE');
+
+function getValue(value) {
+    return { Value: `"${value}"`};
+}
+
+function getResourceRecordSet(domain, resourceRecords=[]) {
+    return {
+      Name: getChallengeDomain(domain),
+      Type: 'TXT',
+      TTL: 60,
+      ResourceRecords: [...resourceRecords]
+    };
+}
+
+function getZoneIDByName(DNSName) {
+  // TODO: handle error
+  return route53.listHostedZonesByName({ DNSName }).promise()
+    .then(result => {
+      // destructure to get to the id returned by listHostedZonesByName
+      const { HostedZones: [ { Id: id } ] } = result;
+      return id;
+    });
+}
+Challenge.set = function (opts, domain, token, keyAuthorization, cb) {
+
+  const keyAuthDigest = base64url.encode(keyAuthorization);
+  if (!token || !keyAuthorization) {
+    console.warn("SANITY FAIL: missing challenge or keyAuthorization", domain, challenge, keyAuthorization);
+  }
+  return getZoneIDByName(domain)
+    .then(id => {
+      const params = route53CreatePayload(id, domain, dig);
+      return route53.changeResourceRecordSets(params)
+        .promise()
+        .then(() => store.setPayload(domain, {
+          id,
+          domain,
+          value: keyAuthDigest
+        }));
+    })
+    .catch(cb);
 };
-Challenge.get = function () { /* TODO */ };
-Challenge.remove = function () { /* TODO */ };
-Challenge.loopback = function () { /* TODO */ };
+Challenge.get = function (opts, domain, token, cb) { /* Not to be implemented */ };
+Challenge.remove = function (opts, domain, token, cb) {
+  // TODO:
+  // this might require something such as an id that needs to be retrieved from a database
+  // ddnsFoo.remove(creds, prefixName(domain), "TXT", token, opts, cb);
+  store.getPayload(domain)
+    .then(({id, domain, value}) => {
+      const params = route53DeletePayload(id, domain, value);
+      return route53.changeResourceRecordSets(params).promise();
+    })
+    .catch(cb);
+};
+Challenge.loopback = function (opts, domain, token, cb) {
+  // var name = '_debug_' + token + prefixName(domain);
+
+  // test that we can actually set the domain record and that we can actually read it
+  // ddnsFoo.set(creds, name, "TXT", token, opts, function (err, result) {
+  //   dns.resolve(name, 'TXT', function (err, records) {
+  //     // now test that token is in records
+  //
+  //     ddnsFoo.remove(creds, name, "TXT", token, opts, cb);
+  //   });
+  // });
+};
